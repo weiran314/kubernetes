5c2b7a38764e85f31d0b7dd69cebda7bc5c27b2c
Darren Shepherd <darren@rancher.com>
2019-02-07 20:53:55 -0700

    Add ability to disable proxy hostname check

--- pkg/registry/core/node/strategy.go
+++ pkg/registry/core/node/strategy.go
@@ -53,6 +53,8 @@ type nodeStrategy struct {
 // objects.
 var Strategy = nodeStrategy{legacyscheme.Scheme, names.SimpleNameGenerator}
 
+var DisableProxyHostnameCheck = false
+
 // NamespaceScoped is false for nodes.
 func (nodeStrategy) NamespaceScoped() bool {
 	return false
@@ -270,6 +272,9 @@ func ResourceLocation(getter ResourceGetter, connection client.ConnectionInfoGet
 }
 
 func isProxyableHostname(ctx context.Context, hostname string) error {
+	if DisableProxyHostnameCheck {
+		return nil
+	}
 	resp, err := net.DefaultResolver.LookupIPAddr(ctx, hostname)
 	if err != nil {
 		return err
